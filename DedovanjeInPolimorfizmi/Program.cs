45fac49eb021ac5d81dc6d0ef961b30f0a956efb
borutluzar <borut.luzar@gmail.com>
2023-01-23 18:25:55 +0100

    Dodano 9. poglavje

---
+++ DedovanjeInPolimorfizmi/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Numerics;
+
+namespace DedovanjeInPolimorfizmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> lstShapes = new List<Shape>()
+            {
+                new Circle(1, 3.4),
+                new Circle(2, 1),
+                new Circle(3, 2),
+                new Rectangle(4, 2, 6),
+                new Rectangle(5, 3, 5.7)
+            };
+
+            foreach(Shape shp in lstShapes)
+            {
+                Console.WriteLine($"Obseg lika {shp.ID} je {shp.Perimeter()}");
+
+                // Če želimo preveriti, ali imamo opravka s posebnim tipom,
+                // to preverimo z rezervirano besedo is
+                if (shp is Circle)
+                {
+                    double area = ((Circle)shp).Area();
+                }
+
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class Bicycle
+    {
+        public int FrameSize { get; set; }
+
+        public int NumGears { get; set; }
+
+        public string Brand { get; set; }
+
+        public int CurrentGear { get; set; }
+
+        public override string ToString()
+        {
+            return $"Velikost okvirja: {FrameSize}\n" +
+                    $"Število prestav: {NumGears}\n" +
+                    $"Znamka: {Brand}";
+        }
+
+        public virtual void ChangeGear(int increaseBy)
+        {
+            this.CurrentGear += increaseBy;
+        }
+    }
+
+    public class EBike : Bicycle
+    {
+        public double BateryTime { get; set; }
+
+        public override string ToString()
+        {
+            // Z rezervirano besedo base povemo, da se sklicujemo na nadrazred.
+            return base.ToString() +
+                    $"Čas vzdržljivosti baterije: {BateryTime}";
+        }
+
+        // Rahlo popravimo metodo iz nadrazreda
+        public override void ChangeGear(int increaseBy)
+        {
+            this.CurrentGear += increaseBy;
+            Console.WriteLine("Zvišanje prestave lahko zmanjša vzdržljivost baterije!");
+        }
+    }
+
+    public class Coffee
+    {
+        public Coffee(double quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public double Quantity { get; set; }
+    }
+
+    public class Turkish : Coffee
+    {
+        // Ob klicu konstruktorja moramo poskrbeti še za
+        // parametre konstruktorja v nadrazredu
+        public Turkish(double quantity, int preparationTime) : base(quantity)
+        {
+            PreparationTime = preparationTime;
+        }
+
+        public int PreparationTime { get; set; }
+    }
+
+
+
+    public class Shape
+    {
+        public Shape(int id)
+        {
+            ID = id;
+        }
+
+        public int ID { get; set; }
+
+        /// <summary>
+        ///  Pripravimo si metodo za izračun obsega, ki jo bomo v podrazredih povozili.
+        /// </summary>
+        public virtual double Perimeter()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(int id, double radius) : base(id)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; set; }
+
+        public override double Perimeter()
+        {
+            return 2 * Radius * Math.PI;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle(int id, double a, double b) : base(id)
+        {
+            A = a;
+            B = b;
+        }
+
+        public double A { get; set; }
+
+        public double B { get; set; }
+
+        public override double Perimeter()
+        {
+            return 2 * A + 2 * B;
+        }
+    }
+}
