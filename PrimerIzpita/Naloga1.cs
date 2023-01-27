8cdb98321df066160bc7d0f025cf5e0b0ddeaf4a
borutluzar <borut.luzar@gmail.com>
2023-01-27 14:28:52 +0100

    Dodano primer izpita.

---
+++ PrimerIzpita/Naloga1.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PrimerIzpita
+{
+    /// <summary>
+    /// NALOGA 1:
+    /// Pripravljate rojstnodnevno zabavo. Kot izkušeni programerji 
+    /// boste pripravili aplikacijo za prijavo gostov.
+    /// Vaša aplikacija (preprosta metoda, ki z uporabnikom komunicira preko ukazne vrstice)
+    /// omogoča vnos imena in priimka (v tem vrstnem redu) vsakega gosta 
+    /// in dodajanje teh podatkov v seznam. [8 točk]
+    /// 
+    /// Ker sumite, da se bo registriralo nekaj nepovabljenih gostov, 
+    /// morate seznam na koncu prečistiti. 
+    /// Iz njega odstranite vse vnose, v katerih ime vsebuje črko T (malo ali veliko),
+    /// in vse vnose, v katerih priimek vsebuje črko N (malo ali veliko). [10 točk]
+    /// 
+    /// Na koncu še izpišite vse goste (iz prečiščenega seznama) in število takšnih,
+    /// ki imajo ime ali priimek daljši od 6 črk. [7 točk]
+    /// </summary>
+    public class Naloga1
+    {
+        /// <summary>
+        /// V tej metodi se začne izvajati program za reševanje Naloge 1
+        /// </summary>
+        public static void ResitevNaloge()
+        {
+
+        }
+    }
+}
---
+++ PrimerIzpita/Naloga4.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PrimerIzpita
+{
+    /// <summary>
+    /// NALOGA 4:
+    /// Napišite metodo Metuljcek, ki za dan parameter n 
+    /// izrise metuljčka. Za vrednost n=5 bi bil izris kot
+    /// na spodnjem primeru:                        [20 točk]
+    /// 
+    /// *       *
+    /// * *   * *
+    /// *   *   *
+    /// * *   * *
+    /// *       *
+    /// 
+    /// V metodi ResitevNaloge pripravite klic metode, ki za parameter
+    /// dobi število n, ki naj ga poda uporabnik.   [5 točk]
+    /// </summary>
+    public class Naloga4
+    {
+        /// <summary>
+        /// V tej metodi se začne izvajati program za reševanje Naloge 4
+        /// </summary>
+        public static void ResitevNaloge()
+        {
+
+        }
+    }
+}
