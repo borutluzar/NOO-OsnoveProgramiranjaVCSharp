971203a5c314990e8775668310f44662526676a9
borutluzar <borut.luzar@gmail.com>
2023-01-27 17:31:22 +0100

    Rešitev 1. naloga

---
+++ PrimerIzpitaResitev/Naloga2.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PrimerIzpitaResitev
+{
+    /// <summary>
+    /// NALOGA 2:
+    /// V metodi ResitevNaloge imate pripravljena dva seznama.
+    /// Prvi je seznam predmetov, drugi pa seznam vpisnih številk študentov.
+    /// 
+    /// Definirana sta tudi razreda Student in Predmet.
+    /// 
+    /// Za vsako vpisno številko pripravite instanco razreda Student,
+    /// ki bo imel dano vpisno številko in seznam vseh petih predmetov,
+    /// pri čemer za vsak predmet oceno izberete slučajno med ocenami
+    /// od 6 do 10. [10 točk]
+    /// 
+    /// V razred Student dodajte metodo, ki izračuna in vrne povprečno oceno študenta. [5 točk]
+    /// 
+    /// Na koncu izpišite povprečno oceno študentov pri vsakem od predmetov. [10 točk]
+    /// </summary>
+    public class Naloga2
+    {
+        /// <summary>
+        /// V tej metodi se začne izvajati program za reševanje Naloge 2
+        /// </summary>
+        public static void ResitevNaloge()
+        {
+            // *** KODE OD TUKAJ DO SPODNJE MEJE NE SPREMINJAJTE!
+
+            // Seznam predmetov
+            List<string> lstImenaPredmetov = new List<string>() 
+            { 
+                "Osnove programiranja",
+                "Informacijski sistemi",
+                "Internet stvari",
+                "Industrijska avtomatizacija",
+                "Kibernetska varnost"
+            };
+
+            // Seznam vpisnih številk študentov
+            List<int> lstVpisneStevilke = new List<int>();
+            for(int i=3530_001; i<=3530_100; i++)
+            {
+                lstVpisneStevilke.Add(i);
+            }
+            // *** KODE DO TUKAJ OD ZGORNJE MEJE NE SPREMINJAJTE!
+
+
+        }
+    }
+
+    public class Student
+    {
+        public Student(int vpSt)
+        {
+            VpisnaStevilka = vpSt;
+            Ocene = new List<Predmet>();
+        }
+
+        public int VpisnaStevilka { get; set; }
+
+        public List<Predmet> Ocene { get; set; }
+    }
+
+    public class Predmet
+    {
+        public Predmet(string naziv, int ocena)
+        {
+            Naziv = naziv;
+            Ocena = ocena;
+        }
+
+        public string Naziv { get; set; }
+        public int Ocena { get; set; }
+    }
+}
---
+++ PrimerIzpitaResitev/Naloga3.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PrimerIzpitaResitev
+{
+    /// <summary>
+    /// NALOGA 3:
+    /// Pripravite razreda Pisalo in Peresnica.
+    /// 
+    /// Razred Pisalo naj vsebuje: 
+    /// -   Lastnosti VrstaPisala (svinčnik, kemični svinčnik, nalivno pero...),
+    ///     CenaPisala, DolžinaPisala, Proizvajalec (tej lastnosti lahko vrednost nastavimo samo v konstruktorju!).
+    /// -   Konstruktor, ki omogoča določitev vsaj dveh lastnosti.
+    /// -   Povozite metodo ToString, ki naj izpiše vse podatke o pisalu. [10 točk]
+    /// 
+    /// Razred Peresnica naj vsebuje:
+    /// -   Lastnosti SeznamPisal in Kapaciteta (t.j., maksimalno število pisal v peresnici).
+    /// -   Konstruktor, ki inicializira seznam pisal.
+    /// -   Metodo, ki izpiše vsa pisala, ki imajo ceno vsaj 3 evre. [10 točk]
+    /// 
+    /// V metodi ResitevNaloge kreirajte instanco razreda Peresnica, ki naj vsebuje vsaj štiri pisala
+    /// in izpišite podatke o vseh pisalih, ki imajo ceno vsaj 3 evre. [5 točk]
+    /// </summary>
+    public class Naloga3
+    {
+        /// <summary>
+        /// V tej metodi se začne izvajati program za reševanje Naloge 3
+        /// </summary>
+        public static void ResitevNaloge()
+        {
+
+        }
+    }
+}
