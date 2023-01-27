b6e73fe24da1055da1b82ed1fc32281b60de7403
borutluzar <borut.luzar@gmail.com>
2023-01-27 19:58:02 +0100

    Popolna rešitev izpita.

--- PrimerIzpitaResitev/Naloga4.cs
+++ PrimerIzpitaResitev/Naloga4.cs
@@ -5,7 +5,7 @@ namespace PrimerIzpitaResitev
 {
     /// <summary>
     /// NALOGA 4:
-    /// Napišite metodo Metuljcek, ki za dan parameter n 
+    /// Napišite metodo Metuljcek, ki za dan lih parameter n 
     /// izrise metuljčka. Za vrednost n=5 bi bil izris kot
     /// na spodnjem primeru:                        [20 točk]
     /// 
@@ -25,7 +25,43 @@ namespace PrimerIzpitaResitev
         /// </summary>
         public static void ResitevNaloge()
         {
+            Console.Write("Vpišite (liho) dimenzijo metuljčka: ");
+            int n = int.Parse(Console.ReadLine());
 
+            /*
+                int.TryParse(Console.ReadLine(), out int test);
+            */
+            // Pokličemo metodo za izris
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Metuljcek(n);
+            Console.ForegroundColor = ConsoleColor.White;
+            // Imamo zadnjih 5 točk!
         }
+
+        private static void Metuljcek(int n)
+        {
+            // Sprehod po vrsticah (n vrstic)
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\t");
+                // Sprehod po stolpcih (2*n stolpcev)
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == 0 || j == n - 1 || i == j || i == (n - 1) - j)
+                    {
+                        // Izpis zvezdic
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        // Izpis presledka
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        // Imamo 20 točk
     }
 }
