b8514413de77717856c2ed71db274197c7cf9320
borutluzar <borut.luzar@gmail.com>
2023-01-27 18:51:02 +0100

    Rešitev 2. naloge

--- PrimerIzpitaResitev/Naloga2.cs
+++ PrimerIzpitaResitev/Naloga2.cs
@@ -29,8 +29,8 @@ namespace PrimerIzpitaResitev
             // *** KODE OD TUKAJ DO SPODNJE MEJE NE SPREMINJAJTE!
 
             // Seznam predmetov
-            List<string> lstImenaPredmetov = new List<string>() 
-            { 
+            List<string> lstImenaPredmetov = new List<string>()
+            {
                 "Osnove programiranja",
                 "Informacijski sistemi",
                 "Internet stvari",
@@ -40,13 +40,56 @@ namespace PrimerIzpitaResitev
 
             // Seznam vpisnih številk študentov
             List<int> lstVpisneStevilke = new List<int>();
-            for(int i=3530_001; i<=3530_100; i++)
+            for (int i = 3530_001; i <= 3530_100; i++)
             {
                 lstVpisneStevilke.Add(i);
             }
             // *** KODE DO TUKAJ OD ZGORNJE MEJE NE SPREMINJAJTE!
 
 
+            // Pripravimo seznam, kamor bomo shranjevali študente
+            List<Student> lstStudents = new List<Student>();
+            // Objekt za generiranje naključnih števil
+            Random rnd = new Random();
+            // Za vsako vpisno številko naredimo nov objekt tipa Student
+            foreach (int vpSt in lstVpisneStevilke)
+            {
+                // Naredimo novo instanco razreda Student
+                Student student = new Student(vpSt);
+
+                // Sprehodimo se po vseh imenih predmetov na seznamu
+                foreach (string nazivPredmeta in lstImenaPredmetov)
+                {
+                    // Izberemo naključno oceno
+                    int ocena = rnd.Next(6, 11);
+                    // Ustvarimo nov objekt tipa predmet
+                    Predmet predmet = new Predmet(nazivPredmeta, ocena);
+                    // in ga dodamo na seznam ocen študenta
+                    student.Ocene.Add(predmet);
+                }
+                lstStudents.Add(student);
+                double povp = student.PovprecnaOcena();
+            }
+            // Imamo 10 točk
+
+
+            // Izračunamo popvrečno oceno pri posameznih predmetih
+
+            // Sprehodimo se po vseh petih predmetih
+            for (int i = 0; i < lstImenaPredmetov.Count; i++)
+            {
+                // Sprehodimo se po seznamu vseh študentov
+                double vsotaOcen = 0;
+                foreach (Student student in lstStudents)
+                {
+                    vsotaOcen += student.Ocene[i].Ocena;
+                }
+                double povprecje = vsotaOcen / lstStudents.Count;
+                // Izpišemo vrednost
+                Console.WriteLine($"Povprečna ocena pri predmetu {lstImenaPredmetov[i]} " +
+                    $"je {povprecje:0.00}");
+            }
+            // Dobimo še 10 točk
         }
     }
 
@@ -61,6 +104,26 @@ namespace PrimerIzpitaResitev
         public int VpisnaStevilka { get; set; }
 
         public List<Predmet> Ocene { get; set; }
+
+        /// <summary>
+        /// Izračuna povprečno oceno študenta
+        /// </summary>
+        public double PovprecnaOcena()
+        {
+            double povprecje = 0;
+
+            // Sprehodimo se po vseh ocenah
+            foreach (Predmet predmet in this.Ocene)
+            {
+                // Prištejemo jih k skupni vsoti
+                povprecje = povprecje + predmet.Ocena;
+            }
+            // Delimo s številom ocen
+            povprecje = povprecje / this.Ocene.Count;
+
+            return povprecje;
+            // Imamo 5 točk
+        }
     }
 
     public class Predmet
@@ -73,5 +136,10 @@ namespace PrimerIzpitaResitev
 
         public string Naziv { get; set; }
         public int Ocena { get; set; }
+
+        public override string ToString()
+        {
+            return $"Predmet: {Naziv}; Ocena: {Ocena}";
+        }
     }
 }
